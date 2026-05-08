1e57e8dd68b3e0d65173fbabc0984cc551694056
luis-hdz7 <luisjorgeantonhernandez@gmail.com>
2026-05-07 18:48:05 -0600

    Renderizando los datos del JSON en el html

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//*DARK MODE
 const btn = document.querySelector(".theme")
 const icon = btn.querySelector(".changeTheme")
 let darkMode=localStorage.getItem("darkMode")
@@ -25,4 +26,32 @@ btn.addEventListener("click", () => {
     } else {
         deshabilitarDarkMode();
     }
+});
+
+//*Guardar la variable
+const iconosCategoria = document.querySelectorAll('.categoria-img');
+iconosCategoria.forEach(iconoCategoria => {
+    iconoCategoria.addEventListener('click', async (evento) => {
+        const seccionEscogida = iconoCategoria.dataset.categoria;
+        const contenedor = document.querySelector('.contenedor-libro');
+        try {
+            const res = await fetch('../js/libros.json');
+            if (!res.ok) throw new Error("No se pudo cargar el archivo JSON");
+            const datos = await res.json();
+            const librosFiltrados = datos.filter(libro => libro.categoria === seccionEscogida);
+            const prefijoRuta = window.location.pathname.includes('paginas/') ? '../' : ''
+            contenedor.innerHTML = librosFiltrados.map(item =>`
+                <div class="item">
+                    <img src="${prefijoRuta}${item.imagen}" alt="Imagen de ${item.titulo}">
+                    <p><strong>${item.titulo}</strong></p>
+                    <ul>
+                        <li><em>${item.autor}</em></li>
+                    </ul>
+                    <button class="btn-agregarCarrito btn-generico">Agregar al Carrito</button>
+                </div>
+            `).join('');
+        } catch (error) {
+            console.error("Hubo un problema:", error);
+        }
+    });
 });
